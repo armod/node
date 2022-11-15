b6ac710db318d0d6401c4129b2f36fd0cc01999d
armod <armods@gmail.com>
2022-11-15 15:48:36 +0100

    update

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,9 +1,22 @@
-const amount = 12
+// const amount = 12
 
-if (amount < 10) {
-  console.log('small number')
-} else {
-  console.log('large number')
+// if (amount < 10) {
+//   console.log('small number')
+// } else {
+//   console.log('large number')
+// }
+
+// console.log(`hey it's my first node app!!!`)
+
+//Modules
+const secret = 'Super Secret'
+const maciek = 'Maciek'
+const maciej = 'Maciej'
+
+const sayHi = (name) => {
+  console.log(`hello there ${name}`)
 }
 
-console.log(`hey it's my first node app!!!`)
+sayHi('Maciej')
+sayHi(maciej)
+sayHi(maciek)
