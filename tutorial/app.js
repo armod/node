bcacc14f4bb9ed374e59a99706ea7d1670b75431
armod <armods@gmail.com>
2022-11-16 09:46:20 +0100

    update

--- tutorial/app.js
+++ tutorial/app.js
@@ -27,3 +27,19 @@
 // OS Modules
 
 const os = require('os')
+
+//info about current user
+const user = os.userInfo()
+console.log(user)
+
+//method returns the system uptime in seconds
+console.log(`The system Uptime is ${os.uptime()} seconds`)
+
+const currentOS = {
+  name: os.type(),
+  realse: os.release(),
+  totalMem: os.totalmem(),
+  freeMem: os.freemem(),
+}
+
+console.log(currentOS)
