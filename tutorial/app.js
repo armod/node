88b6022f61bf7f0d2fe80958f9664435bd4c85f0
armod <armods@gmail.com>
2022-11-16 09:52:29 +0100

    update

--- tutorial/app.js
+++ tutorial/app.js
@@ -26,20 +26,34 @@
 
 // OS Modules
 
-const os = require('os')
+// const os = require('os')
 
-//info about current user
-const user = os.userInfo()
-console.log(user)
+// //info about current user
+// const user = os.userInfo()
+// console.log(user)
 
-//method returns the system uptime in seconds
-console.log(`The system Uptime is ${os.uptime()} seconds`)
+// //method returns the system uptime in seconds
+// console.log(`The system Uptime is ${os.uptime()} seconds`)
 
-const currentOS = {
-  name: os.type(),
-  realse: os.release(),
-  totalMem: os.totalmem(),
-  freeMem: os.freemem(),
-}
+// const currentOS = {
+//   name: os.type(),
+//   realse: os.release(),
+//   totalMem: os.totalmem(),
+//   freeMem: os.freemem(),
+// }
 
-console.log(currentOS)
+// console.log(currentOS)
+
+// PATH Modules
+const path = require('path')
+
+console.log(path.sep)
+
+const filePath = path.join('/content', 'subfolder', 'test.txt')
+console.log(filePath)
+
+const base = path.basename(filePath)
+console.log(base)
+
+const absolute = path.resolve(__dirname, 'content', 'subfolder', 'test.txt')
+console.log(absolute)
