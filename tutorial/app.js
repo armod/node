5fc028c45a17f64995e4381465c04aea28428d87
armod <armods@gmail.com>
2022-11-17 21:10:20 +0100

    update

--- tutorial/app.js
+++ tutorial/app.js
@@ -127,8 +127,11 @@ const EventEmitter = require('events')
 
 const customEmitter = new EventEmitter()
 
+customEmitter.on('response', (name, id) => {
+  console.log(`data recived ${name} with id: ${id}`)
+})
 customEmitter.on('response', () => {
-  console.log(`data recived`)
+  console.log(`some other logic here`)
 })
 
-customEmitter.emit('response')
+customEmitter.emit('response', 'john', 34)
