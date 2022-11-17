47969664283c02f077d2a332cefdce3527c7e944
armod <armods@gmail.com>
2022-11-17 21:26:39 +0100

    update

--- tutorial/app.js
+++ tutorial/app.js
@@ -123,15 +123,38 @@
 //   console.log('Server listen on porst: 5000')
 // })
 
-const EventEmitter = require('events')
+// const EventEmitter = require('events')
 
-const customEmitter = new EventEmitter()
+// const customEmitter = new EventEmitter()
 
-customEmitter.on('response', (name, id) => {
-  console.log(`data recived ${name} with id: ${id}`)
-})
-customEmitter.on('response', () => {
-  console.log(`some other logic here`)
-})
+// customEmitter.on('response', (name, id) => {
+//   console.log(`data recived ${name} with id: ${id}`)
+// })
+// customEmitter.on('response', () => {
+//   console.log(`some other logic here`)
+// })
 
-customEmitter.emit('response', 'john', 34)
+// customEmitter.emit('response', 'john', 34)
+
+// Streams
+// create big file
+// const { writeFileSync } = require('fs')
+// for (let i = 0; i < 10000; i++) {
+//   writeFileSync('./content/big.txt', `hellow world ${i}\n`, { flag: 'a' })
+// }
+
+// const { createReadStream } = require('fs')
+
+// // const stream = createReadStream('./content/big.txt', 'utf8')
+// const stream = createReadStream('./content/big.txt', {
+//   highWaterMark: 90000,
+//   encoding: 'utf8',
+// })
+
+// stream.on('data', (result) => {
+//   console.log(result)
+// })
+
+// stream.on('error', (err) => {
+//   console.log(err)
+// })
